6fd6a9cc59978609900e0923e2dfb374a9aeef46
Charles Mwirigi <mwirigicharles@gmail.com>
2018-12-12 01:22:39 +0300

    styled the error message for the app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,3 +28,11 @@ $(window).scroll(function(){
         x = $('#nav-custom').removeClass('shadow-header');
     }
 });
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('.notice-wrapper').fadeOut("slow", function(){
+      $this.remove();
+    });
+  }, 2500);
+});
